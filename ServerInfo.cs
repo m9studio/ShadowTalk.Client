e40eac2c04794ac54df097605437e5fc936a96fa
m9 studio <m9.agrest@gmail.com>
2025-05-13 00:40:16 +0300

    Create ServerInfo.cs

---
+++ ServerInfo.cs
@@ -0,0 +1,22 @@
+﻿namespace M9Studio.ShadowTalk.Client
+{
+    internal class ServerInfo
+    {
+        public string IP;
+        public int Port;
+
+
+
+
+        //для расшифровки сообщений
+        public string RSA;
+
+        public string Name;
+        public int Id;
+
+
+        public string K;
+        public string token;
+        public string hmac;
+    }
+}
